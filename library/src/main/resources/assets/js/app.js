b1a3c6bfa0fe8596d9ecf0295e65f71c134c1d13
kavishparikh <kavishb.parikh@gmail.com>
2013-11-10 23:03:53 -0800

    Lab 4 - Solution

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,23 @@
 $(":button").click(function() {
 	var isbn = this.id;
-	alert('About to report lost on ISBN ' + isbn);
+	var URL = "/library/v1/books/"+isbn+"?status=lost";
+    alert('About to report lost on ISBN #' + isbn);
+	$.ajax({
+		type: "PUT",
+	    url: URL,
+	    contentType: "application/json",
+	    success: function(isbn) {
+            alert('The book is reported lost'); 
+            window.location.reload(); 
+		},
+	});
+});
+
+$(function(){ 
+	$('button').each(function(){
+		if(this.value == "lost")
+		{
+			$(this).prop('disabled', true);
+		}
+	 });
 });
